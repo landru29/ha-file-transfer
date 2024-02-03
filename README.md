0f3753699ef19ec2289acbc066e54145946e8e40
Cyrille Meichel <cyrille.meichel@gmail.com>
2024-02-03 13:00:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Development
+
+https://www.hacf.fr/dev_tuto_1_integration/
---
+++ custom_components/file_transfer/const.py
@@ -0,0 +1,6 @@
+""" Les constantes pour l'intégration Tuto HACS """
+
+from homeassistant.const import Platform
+
+DOMAIN = "file_transfer"
+PLATFORM: list[Platform] = []
